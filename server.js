06b6339af1e2c4d005c5a6b8ca48705cb7dadf25
Victor <viknavarro@gmail.com>
2022-08-04 12:44:54 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+mongoose.connect(
+    `mongodb://localhost:27017/user-authentication`,
+    () => console.log("Connected to Mongo DB")
+);
+
+app.use("/todo", require("./routes/todoRouter"));
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.send({errMsg: err.message});
+});
+
+app.listen(9000, () => {
+    console.log("Server running on the local port 9000")
+});
+
+
